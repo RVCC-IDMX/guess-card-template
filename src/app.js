63d06a9401cb5339566e5080073e74b431edb13f
cynthiateeters <glean@cynthiateeters.com>
2021-02-12 18:42:11 -0500

    Finish

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@
 
 /* 
   DO NOT FORGET
-Edit random.js to so getRandomInt(max)
+Edit random.js so getRandomInt(max)
 will return a random number from 
 0 to the value of max (inclusively)
 
 */
-import { getRandomInt } from "./random.js"; //using the ES6 modules
+import { getRandomInt } from './random.js'; //using the ES6 modules
 
-const farewell = document.querySelector(".farewell");
+const farewell = document.querySelector('.farewell');
 const suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs',];
 
 while (confirm('Would you like to play the game?')) { 
@@ -18,4 +18,4 @@ while (confirm('Would you like to play the game?')) {
 
 }
 
-farewell.innerHTML = "<h1>Thanks for playing!<h1>";
+farewell.innerHTML = '<h1>Thanks for playing!<h1>';
